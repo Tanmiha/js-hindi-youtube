42fdcf5a3adc6dd37b1586e5f3e6518a0c597235
Tanmiha <mtanmiha@gmail.com>
2025-06-03 13:16:29 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3+3);
+
+console.log("Tanmiha");
+
+
+let name = "tanmiha"
+let age = 21
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);  // iska type object hota hai
